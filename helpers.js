52ff1b6a49cc829ecbda4474fc6cfb23c74abcbb
Glitch (hello-webpage) <none>
2019-02-21 00:22:47 +0000

    💓🍰 Checkpoint ./index.html:293015/2441 ./style.css:293015/3080 ./script.js:293015/2815 ./mm_bundle.js:293015/9804205 ./magentamusic.js:293015/1659005 ./helpers.js:293015/5412

---
+++ helpers.js
@@ -0,0 +1,181 @@
+
+/***********************************
+ * Board of dots
+ ***********************************/
+const PIXELS_WIDTH = 32;
+const PIXELS_HEIGHT = 46;
+//const MIN_PITCH = 36;
+const MAX_PITCH = 81;
+// Soprano, Alto, Tenor, Bass.
+const RANGES = [[60, 81], [52, 74], [46, 69], [36, 66]];
+
+class Board {
+  constructor() {
+    this.data = [];
+    this.ui = {}; // Gets populated by this.reset().
+    this.reset();
+    this.isPlaying = false;
+  }
+
+  reset() {
+    this.data = [];
+    this.ui.container = document.getElementById('container');
+    this.ui.container.innerHTML = '';
+
+    // Recreate the board.
+    for (let i = 0; i < PIXELS_HEIGHT; i++) {
+      this.data.push([]);
+      const rowEl = document.createElement('div');
+      rowEl.classList.add('row');
+      this.ui.container.appendChild(rowEl);
+
+      for (let j = 0; j < PIXELS_WIDTH; j++) {
+        this.data[i][j] = {};
+        const button = document.createElement('button');
+        button.setAttribute('aria-label', 'cell, empty');
+        button.classList.add('pixel');
+        button.dataset.row = i;
+        button.dataset.col = j;
+
+        // Add a pitch label.
+        if (j === 0) {
+          const span = document.createElement('span');
+          span.textContent = MAX_PITCH - i;
+          rowEl.appendChild(span);
+        }
+        rowEl.appendChild(button);
+      }
+      // Add a voice label.
+      for (let v = 0; v < RANGES.length; v++) {
+        const pitch = MAX_PITCH - i;
+        if (this.isPitchInRange(pitch, v)) {
+          const span = document.createElement('span');
+          span.setAttribute('class', `pixel voice${v}`);
+          rowEl.appendChild(span);
+        }
+      }
+    }
+    this.ui.rows = document.querySelectorAll('.container > .row');
+  }
+
+  // Toggles a particular dot from on to off.
+  toggleCell(i, j, uiButton, voiceOverride) {
+    const dot = this.data[i][j];
+    const pitch = MAX_PITCH - i;
+
+    // Init this cell if it's never been set before.
+    if (dot.on === undefined) dot.on = -1;
+    if (voiceOverride === -1) {
+      dot.on = this.getNextVoice(pitch, dot.on)
+    } else {
+      // We have an override. We should try to use it, unless we're drawing
+      // out of range, in which case default to the next available voice.
+      if (this.isPitchInRange(pitch, voiceOverride)) {
+        dot.on = voiceOverride;
+      } else {
+        const nextVoice = this.getNextVoice(pitch, voiceOverride);
+        dot.on = nextVoice === -1 ? this.getNextVoice(pitch, -1) : nextVoice;
+      }
+    }
+
+    if (dot.on === -1) {
+      this.resetButton(uiButton);
+    } else {
+      this.voiceButton(uiButton, dot.on);
+    }
+  }
+
+  resetButton(uiButton) {
+    uiButton.setAttribute('class', 'pixel');
+    uiButton.setAttribute('aria-label', 'cell, empty');
+  }
+
+  voiceButton(uiButton, voice) {
+    const aria = voice === 0 ? 'soprano' : voice === 1 ? 'alto' : voice === 2 ? 'tenor' : 'bass';
+    uiButton.setAttribute('aria-label', `cell, ${aria}`);
+    uiButton.setAttribute('class', `pixel voice${voice}`);
+  }
+
+  getNoteSequence() {
+    const sequence = {notes:[], quantizationInfo: {stepsPerQuarter: 4}};
+    for (let i = 0; i < PIXELS_HEIGHT; i++) {
+      for (let j = 0; j < PIXELS_WIDTH; j++) {
+        // This note is on.
+        if (this.data[i][j].on >= 0) {
+          sequence.notes.push(
+            { pitch: 81 - i,
+              instrument: this.data[i][j].on,
+              quantizedStartStep: j,
+              quantizedEndStep: j + 1
+            },
+          );
+        }
+      }
+    }
+    sequence.totalQuantizedSteps = sequence.notes[sequence.notes.length - 1].quantizedEndStep;
+    return sequence;
+  }
+
+  drawNoteSequence(ns) {
+    console.log(ns.totalQuantizedSteps);
+    this.reset();
+    for(let n = 0; n < ns.notes.length; n++) {
+      const note = ns.notes[n];
+      const r = MAX_PITCH - note.pitch;
+      const c = note.quantizedStartStep
+      const v = note.instrument
+      this.data[r][c].on = v;
+      const uiButton = document.querySelector(`.pixel[data-row="${r}"][data-col="${c}"]`);
+      this.voiceButton(uiButton, v);
+    }
+  }
+
+  getNextVoice(pitch, thisVoice) {
+    for (let v = thisVoice + 1; v < RANGES.length; v++) {
+      if (this.isPitchInRange(pitch, v)) {
+        return v;
+      }
+    }
+    return -1;
+  }
+
+  isPitchInRange(pitch, voice) {
+    if (RANGES[voice] === undefined) {
+      return false;
+    }
+    return RANGES[voice][0] <= pitch && RANGES[voice][1] >= pitch;
+  }
+
+  playStep(note) {
+    const v = note.instrument;
+    const r = MAX_PITCH - note.pitch;
+    const c = note.quantizedStartStep;
+
+    const on =  document.querySelectorAll('.pixel.active, .pixel.bar');
+    for (let p = 0; p < on.length; p++) {
+      on[p].classList.remove('bar');
+      if (on[p].dataset.col < c) {
+        on[p].classList.remove('active');
+      }
+    }
+
+    // const bars = document.querySelectorAll(`.pixel[data-col="${c}"]`);
+    // for (let p = 0; p < bars.length; p++) {
+    //   bars[p].classList.add('bar');
+    // }
+
+    const pixels = document.querySelectorAll(`.pixel.voice${v}[data-row="${r}"][data-col="${c}"]`);
+    for (let p = 0; p < pixels.length; p++) {
+      pixels[p].classList.remove('bar');
+      pixels[p].classList.add('active');
+    }
+  }
+
+  playEnd() {
+    const on =  document.querySelectorAll('.pixel.active, .pixel.bar');
+    for (let p = 0; p < on.length; p++) {
+      on[p].classList.remove('bar');
+      on[p].classList.remove('active');
+    }
+  }
+}
--- script.js
+++ script.js
@@ -1,5 +1,85 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+const board = new Board();
+const player = new mm.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
+const model = new mm.Coconet('https://storage.googleapis.com/magentadata/js/checkpoints/coconet/bach');
+model.initialize();
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+let isMouseDown = false;
+let isAnimating = false;
+let forceVoiceDrawing = -1;
+
+init();
+
+function init() {
+  player.callbackObject = {
+    run: (note) => board.playStep(note),
+    stop: () => {
+      btnPlay.textContent = 'Play';
+      board.playEnd();
+      document.getElementById('container').classList.remove('playing');
+    }
+  };
+
+  // Set up event listeners.
+  const container = document.getElementById('container');
+  container.addEventListener('mousedown', (event) => { isMouseDown = true; clickCell(event) });
+  container.addEventListener('mouseup', () => isMouseDown = false);
+  container.addEventListener('mouseover', clickCell);
+}
+
+function clickCell(event) {
+  const button = event.target;
+
+  // We only care about clicking on the buttons, not the container itself.
+  if (button.localName !== 'button' || !isMouseDown) {
+    return;
+  }
+  const x = parseInt(button.dataset.row);
+  const y = parseInt(button.dataset.col);
+  board.toggleCell(x, y, button, forceVoiceDrawing);
+}
+
+function reset() {
+  board.reset();
+}
+
+function playOrPause() {
+  const container = document.getElementById('container');
+
+  if (isAnimating) {
+    container.classList.remove('playing');
+    player.stop();
+  } else {
+    container.classList.add('playing');
+    const sequence = board.getNoteSequence();
+  //  player.setTempo(parseFloat(inputTempo.value));
+    player.loadSamples(sequence).then(() => {
+      player.start(sequence);;
+    });
+  }
+  isAnimating = !isAnimating;
+  btnPlay.textContent = isAnimating? 'Stop' : 'Play';
+}
+
+function infill() {
+  const sequence = board.getNoteSequence();
+  model.infill(sequence).then((output) => board.drawNoteSequence(output));
+}
+
+function activateVoice(event, voice) {
+  const parentLabel = event.target.parentElement;
+
+  // If we're clicking an activated button, then we're really deactivating it.
+  if (parentLabel && parentLabel.classList.contains('active')) {
+    parentLabel.classList.remove('active');
+    forceVoiceDrawing = -1;
+  } else {
+    // Deactivate the previous label.
+    const prevButton = document.querySelector('label.active');
+    if (prevButton) {
+      prevButton.classList.remove('active');
+    }
+    // Activate this one.
+    parentLabel.classList.add('active');
+    forceVoiceDrawing = voice;
+  }
+}
